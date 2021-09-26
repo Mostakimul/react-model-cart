8da6b980cd6ccfc377a30a6740acdf1d3fa7b811
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-09-26 15:20:20 +0200

    edited readme

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="row bg-light rounded-3 shadow-lg py-3 my-3">
       <div className="col-12">
-        <h2 className="text-center">Fair & Handsome</h2>
+        <h2 className="text-center fw-bold">Fair & Handsome</h2>
         <h4 className="text-center">Total Budget: $8000</h4>
       </div>
     </div>
